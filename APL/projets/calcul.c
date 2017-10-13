53caa35e721069112e52e7c67a7324d805693dfc
BuildTools <unconfigured@null.spigotmc.org>
2017-10-13 17:58:21 +0200

    calcul.c

--- APL/projets/calcul.c
+++ APL/projets/calcul.c
@@ -38,7 +38,7 @@ void soustr(int tab1[], int tab2[], int result[]) {
 
 void printtab(int tab[]) {
     int i;
-    for(i=0; i<99; i++) {
+    for(i=0; i<102; i++) {
         printf("%d", tab[i]);
     }
     printf("\n");
@@ -71,26 +71,26 @@ void calcul(int tab1[], int tab2[], int tabresult[], char op) {
 
 void main() {
 
-    int tab1[100]= {0};
-    int tab2[100]= {0};
-    int tab3[100]= {0};
+    int tab1[102]= {0};
+    int tab2[102]= {0};
+    int tab3[102 ]= {0};
     int tabresult[1000]= {0};
     int tabfinal[1000]= {0};
     char op1, op2, var;
-    int i=2;
+    int i=1;
 
     //Attribution 0/1
     scanf("%c", &var);
     if(var == '-') {
-        tab1[0]=1;
+        tab1[100]=1;
         //Skip espaces entre négatif et 1er nombre
         do {
             scanf("%c", &var);
         } while(var == ' ');
-        tab1[1]=var - '0';
+        tab1[0]=var - '0';
     } else {
-        tab1[0]=0;
-        tab1[1]=var - '0';
+        tab1[100]=2;
+        tab1[0]=var - '0';
     }
 
     //Remplissage 1er tab
@@ -101,7 +101,6 @@ void main() {
         i++;
         scanf("%c", &var);
     }
-    tab1[i]=11111;
 
     //Skip espaces et récupération 1er opérateur
     while(var == ' ') {
@@ -120,17 +119,17 @@ void main() {
     } while(var == ' ');
 
     //Attribution 0/1
-    i=2;
+    i=1;
     if(var == '-') {
-        tab2[0]=1;
+        tab2[100]=1;
         //Skip espaces entre négatif et 2e nombre
         do {
             scanf("%c", &var);
         } while(var == ' ');
-        tab2[1]=var - '0';
+        tab2[0]=var - '0';
     } else {
-        tab2[0]=0;
-        tab2[1]=var - '0';
+        tab2[100]=2;
+        tab2[0]=var - '0';
     }
 
     //Remplissage 2e tab
@@ -140,7 +139,6 @@ void main() {
         i++;
         scanf("%c", &var);
     }
-    tab2[i]=11111;
 
     //Skip espaces et récupération 2e opérateur
     while(var == ' ') {
@@ -166,17 +164,17 @@ void main() {
     } while(var == ' ');
 
     //Attribution 0/1
-    i=2;
+    i=1;
     if(var == '-') {
-        tab3[0]=1;
+        tab3[100]=1;
         //Skip espaces entre négatif et 3e nombre
         do {
             scanf("%c", &var);
         } while(var == ' ');
-        tab3[1]=var - '0';
+        tab3[0]=var - '0';
     } else {
-        tab3[0]=0;
-        tab3[1]=var - '0';
+        tab3[100]=2;
+        tab3[0]=var - '0';
     }
 
     //Remplissage 3e tab
@@ -186,7 +184,6 @@ void main() {
         i++;
         scanf("%c", &var);
     }
-    tab1[i]=11111;
 
     printtab(tab3);
 
