8c3b6d8a81177bbc51ee878be99b6db54e149fc8
Matthieu <matthieu-petit@laposte.net>
2017-12-06 16:39:19 +0100

    auto-commit raspberry

--- APL/projets/Snake/main.c
+++ APL/projets/Snake/main.c
@@ -9,104 +9,118 @@
 
 int forcexit(int touche) {
 
-	if(touche == XK_Escape)
-		return 1;
-	else
-		return 0;
+  if(touche == XK_Escape)
+    return 1;
+  else
+    return 0;
 }
 
-void move_forward (Body *B, int nbr) {
+void move_forward (Body *B) {
 
-	EffacerEcran(CouleurParNom("light grey"));
-	ChoisirCouleurDessin(CouleurParNom("yellow"));
+  EffacerEcran(CouleurParNom("yellowgreen"));
+  ChoisirCouleurDessin(CouleurParNom("khaki"));
 
-	int i;
-	for(i = nbr-1 ; i >= 1 ; i--) {
-		B->s_seg[i] = B->s_seg[i-1];
-	}
+  B->s_seg[B->nbrseg] = B->s_seg[B->nbrseg-1];
+  int i;
+  for(i = B->nbrseg-1 ; i >= 1 ; i--) {
+    B->s_seg[i] = B->s_seg[i-1];
+  }
 
-	switch(B->s_dir) {
-	case 1:
-		B->s_seg[0].y-=12;
-		break;
-	case 2:
-		B->s_seg[0].y+=12;
-		break;
-	case 3:
-		B->s_seg[0].x-=12;
-		break;
-	case 4:
-		B->s_seg[0].x+=12;
-		break;
-	}
-
-	for(i = 0 ; i < nbr ; i++) {
-		RemplirRectangle(B->s_seg[i].x, B->s_seg[i].y, 10, 10);
-		//printf("Coords [%d]: %d | %d\n", i, B->s_seg[i].x, B->s_seg[i].y);
-	}
+  switch(B->s_dir) {
+  case 1:
+    B->s_seg[0].y-=12;
+    break;
+  case 2:
+    B->s_seg[0].y+=12;
+    break;
+  case 3:
+    B->s_seg[0].x-=12;
+    break;
+  case 4:
+    B->s_seg[0].x+=12;
+    break;
+  }
 }
 
-void body_init (Body *B, int nbr) {
-	int i;
-	int posx = POSX;
-	int posy = POSY;
-	for(i = 0 ; i < nbr ; i++) {
-		B->s_seg[i].x = posx;
-		B->s_seg[i].y = posy;
-		posx-=12;
-		posy-=12;
-	}
-	B->s_dir = 2;
+void draw (Body B) {
+
+  int i;
+  for(i = 0 ; i < B.nbrseg ; i++) {
+    RemplirRectangle(B.s_seg[i].x, B.s_seg[i].y, 10, 10);
+  }
 }
 
-int verifhead (int x, int y) {
+void body_init (Body *B) {
+  int i;
+  int posx = 20;
+  int posy = 30;
+  for(i = 0 ; i < B->nbrseg ; i++) {
+    B->s_seg[i].x = posx;
+    B->s_seg[i].y = posy;
+    posx-=12;
+  }
+  B->s_dir = 4;
+}
 
-	if(x <= 10 || x >= WIDTH-10 || y <= 10 || y >= HEIGHT-10) {
-		//printf("Coords: %d | %d", x, y);
-		return 1;
-	}
-	return 0;
+int verif (Body S) {
+
+  int x = S.s_seg[0].x;
+  int y = S.s_seg[0].y;
+  if(x <= 10 || x >= WIDTH-10 || y <= 10 || y >= HEIGHT-10) {
+    //printf("Coords: %d | %d", x, y);
+    return 1;
+  }
+
+  int i;
+  for(i = 4 ; i <= S.nbrseg ; i++){
+    if(x == S.s_seg[i].x && y == S.s_seg[i].y)
+      return 1;
+  }
+ 
+  return 0;
 }
 
 int main () {
 
-	InitialiserGraphique();
-	CreerFenetre(500,300,WIDTH,HEIGHT);
-	EffacerEcran(CouleurParNom("light grey"));
+  InitialiserGraphique();
+  CreerFenetre(500,300,WIDTH,HEIGHT);
+  EffacerEcran(CouleurParNom("light grey"));
 
-	Body snake_body;
-	snake_body.s_seg = malloc(4 * sizeof(Segment));
-	body_init(&snake_body, 4);
+  Body snake_body;
+  snake_body.nbrseg = 14;
+  snake_body.s_seg = malloc((snake_body.nbrseg+1) * sizeof(Segment));
+  body_init(&snake_body);
 
-	int touche, old_dir = 2;
+  int touche, old_dir = 4;
 
-	while(!forcexit(touche)) {
+  while(!forcexit(touche)) {
 
-		while(!ToucheEnAttente() && !verifhead(snake_body.s_seg[0].x, snake_body.s_seg[0].y)) {
-			move_forward(&snake_body, 4);
-			usleep(150000);
-		}
+    while(!ToucheEnAttente() && !verif(snake_body)) {
+      usleep(150000);
+      move_forward(&snake_body);
+      draw(snake_body);
+    }
 
-		if(verifhead(snake_body.s_seg[0].x, snake_body.s_seg[0].y))
-			break;
+    if(verif(snake_body))
+      break;
 
-		touche = Touche();
+    touche = Touche();
 
-		if(touche == XK_Up)
-			snake_body.s_dir = UP;
-		if(touche == XK_Down)
-			snake_body.s_dir = DOWN;
-		if(touche == XK_Left)
-			snake_body.s_dir = LEFT;
-		if(touche == XK_Right)
-			snake_body.s_dir = RIGHT;
+    if(touche == XK_Up)
+      snake_body.s_dir = UP;
+    if(touche == XK_Down)
+      snake_body.s_dir = DOWN;
+    if(touche == XK_Left)
+      snake_body.s_dir = LEFT;
+    if(touche == XK_Right)
+      snake_body.s_dir = RIGHT;
 
-		if(old_dir+snake_body.s_dir == 3 || old_dir+snake_body.s_dir == 7)
-			snake_body.s_dir = old_dir;
+    if(old_dir+snake_body.s_dir == 3 || old_dir+snake_body.s_dir == 7)
+      snake_body.s_dir = old_dir;
 
-		old_dir = snake_body.s_dir;
-	}
+    old_dir = snake_body.s_dir;
+  }
 
-	FermerGraphique();
-	return EXIT_SUCCESS;
+  FermerGraphique();
+  return EXIT_SUCCESS;
 }
--- APL/projets/Snake/snake.h
+++ APL/projets/Snake/snake.h
@@ -2,19 +2,20 @@
 #define POSY 70
 
 typedef enum {
-	UP = 1,
-	DOWN,
-	LEFT,
-	RIGHT
+  UP = 1,
+  DOWN,
+  LEFT,
+  RIGHT
 } Direction;
 
 typedef struct {
-	int x;
-	int y;
+  int x;
+  int y;
 } Segment;
 
 typedef struct {
-	Segment *s_seg;
-	Direction s_dir;
-	int speed;
+  Segment *s_seg;
+  Direction s_dir;
+  int speed;
+  int nbrseg;
 } Body;
