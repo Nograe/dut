93d8acd6d214969ea290453ab942a6a67a3c4f9c
Matthieu <matthieu-petit@laposte.net>
2017-12-06 14:55:55 +0100

    auto-commit raspberry

--- APL/SDL/5.c
+++ APL/SDL/5.c
@@ -37,7 +37,8 @@ int main() {
   int touche = 0, x = 0, y = 0, c = 0;
   int target = 0, target_y, target_x;
   char buf[100];
-  unsigned long temps = Microsecondes() + seconde;
+  unsigned long temps = Microsecondes();
+  unsigned long curtime = 0;
   srand(time(NULL));
 
   x = WIDTH/2-15;
@@ -49,7 +50,7 @@ int main() {
   EffacerEcran(CouleurParNom("light grey"));
   ChoisirEcran(0);
 
-  while(target != 10){
+  while(target != 4){
 
     if (ToucheEnAttente()){
       touche = Touche();
@@ -75,34 +76,47 @@ int main() {
 	}
       } */
       
-      
-      touche = Touche();
-
-      EffacerEcran(CouleurParNom("light grey"));
-      
-      if(touche == XK_Left)
-	x-=5;
-      if(touche == XK_Right)
-	x+=5;
-      if(touche == XK_Up)
-	y-=5;
-      if(touche == XK_Down)
-	y+=5;
-      if(touche == XK_space && x >= target_x-30 && x <= target_x+30 && y >= target_y-30 && y <= target_y+30){
-	target++;
-	break;
+      if (ToucheEnAttente()) {
+	touche = Touche();
+	
+	if(touche == XK_Left)
+	  x-=5;
+	if(touche == XK_Right)
+	  x+=5;
+	if(touche == XK_Up)
+	  y-=5;
+	if(touche == XK_Down)
+	  y+=5;
+	if(touche == XK_space && x >= target_x-30 && x <= target_x+30 && y >= target_y-30 && y <= target_y+30){
+	  target++;
+	  break;
+	}
       }
 
+      ChoisirEcran(1);
+      EffacerEcran(CouleurParNom("light grey"));
+
       draw_target(target_x, target_y);
+      
       draw_viseur(x, y);
+
+      unsigned long tmp = (Microsecondes() - temps)/1000000;
+      sprintf(buf,"%d",tmp);
+      EcrireTexte(WIDTH-20,HEIGHT-20,buf,1);
+      sprintf(buf,"%d",target);
+      EcrireTexte(20, HEIGHT-20, buf, 1);
+      CopierZone(1, 0, 0, 0, WIDTH, HEIGHT, 0, 0);
+
+      usleep(100);
     }
   }
 
+  ChoisirEcran(0);
   EffacerEcran(CouleurParNom("light grey"));
   temps = Microsecondes() - temps;
   snprintf(buf,100,"Nombre de cibles: %d  Votre temps: %03ld", target, temps/1000000);
   EcrireTexte(20, HEIGHT/2, buf, 2);
-  sleep(5);
+  sleep(1);
   FermerGraphique();
   return EXIT_SUCCESS;
 
