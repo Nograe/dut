4019bc8c34ceb8f5ef5a810e76bbda5c740aac8c
NograeUbuntu <matthieu@ubuntu>
2018-01-30 23:17:50 +0100

    auto-commit matthieu@ubuntu

--- ASR/TP9/bin2dot-with-read.sh
+++ ASR/TP9/bin2dot-with-read.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 if [[ $# -lt 2 ]]
-then
+    then
     echo "Usage: $0 <src_file> <dest_file>"
     exit
 fi
 if [[ -f $2 ]]
-then
+    then
     echo -n "$2 exists. Overwrite? Yes/No --> "
     read answer
     if [ $answer != "Yes" ] && [ $answer != "yes" ]
-    then
-	exit
+        then
+        exit
     fi
     cp /dev/null $2
 fi
-for addr in $(cat $1)
+while read c
 do
-    x=$(expr substr $addr 1 8)
-    y=$(expr substr $addr 9 8)
-    z=$(expr substr $addr 17 8)
-    t=$(expr substr $addr 25 8)
+    echo "c: $c"
+    x=$(expr substr $c 1 8)
+    y=$(expr substr $c 9 8)
+    z=$(expr substr $c 17 8)
+    t=$(expr substr $c 25 8)
     addr_dot=$((9#$x)).$((9#$y)).$((9#$z)).$((9#$t))
     echo $addr_dot >> $2
 done
