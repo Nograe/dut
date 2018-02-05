1d701cb5a7f9addb3bfad004e18092055e99e921
NograeUbuntu <matthieu@ubuntu>
2018-02-05 22:23:26 +0100

    auto-commit matthieu@ubuntu

--- APL/Java/1/ex2.java
+++ APL/Java/1/ex2.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class ex2 {
-    public static void main (String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int tab[] = new int[5];
-	tab[0] = sc.nextInt();
-	tab[1] = sc.nextInt();
-	tab[2] = sc.nextInt();
-	tab[3] = sc.nextInt();
-	tab[4] = sc.nextInt();
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tab[] = new int[5];
+		tab[0] = sc.nextInt();
+		tab[1] = sc.nextInt();
+		tab[2] = sc.nextInt();
+		tab[3] = sc.nextInt();
+		tab[4] = sc.nextInt();
 
-	int grand = tab[0];
-	for(int i = 1; i < tab.length; i++) {
-	    if (tab[i] > grand) {
-		grand = tab[i];
-	    }
+		int grand = tab[0];
+		for (int i = 1; i < tab.length; i++) {
+			if (tab[i] > grand) {
+				grand = tab[i];
+			}
+		}
+
+		System.out.println(Arrays.toString(tab));
+		System.out.println("Plus grand entier: " + grand);
 	}
-
-	System.out.println(Arrays.toString(tab));
-	System.out.println("Plus grand entier: " + grand);
-    }
 }
--- APL/Java/1/ex7.java
+++ APL/Java/1/ex7.java
@@ -1,34 +1,57 @@
 import java.util.*;
 
 class Matrix {
-    int tab[][];
+   int tab[][];
 
-    Matrix (int N, int M) {
-	tab = new int[N][M];
-	Random rand = new Random(); 
-	 
-	for(int i = 0; i < N; i++) {
-	    for(int j = 0; j < M; j++)
-		tab[i][j] = rand.nextInt(100);
-	}
-    }
+   Matrix (int N, int M) {
+      tab = new int[N][M];
+      Random rand = new Random();
 
-    void copy (int origin[], int dim) {
-	for(int i = 0; i < origin.length; i++)
-	    tab[dim][i] = origin[i];
-    }
-    
-    void addition (int mat[]) {
-	
-    }
+      for (int i = 0; i < N; i++) {
+         for (int j = 0; j < M; j++)
+            tab[i][j] = rand.nextInt(100);
+      }
+   }
+
+   void print () {
+      for (int i = 0; i < this.tab.length; i++)
+         System.out.println(Arrays.toString(this.tab[i]));
+   }
+
+   void copy (int origin[], int dim) {
+      for (int i = 0; i < this.tab[0].length; i++)
+         this.tab[dim][i] = origin[i];
+   }
+
+   void addition (int add[][]) {
+      for (int i = 0; i < this.tab.length; i++) {
+         for (int j = 0; j < this.tab[0].length; j++) {
+            this.tab[i][j] += add[i][j];
+         }
+      }
+   }
 }
 
 public class ex7 {
 
-    public static void main (String[] args) {
-	Matrix matrice;
-	matrice = new Matrix(2, 4);
+   public static void main (String[] args) {
+      Matrix matrice;
+      matrice = new Matrix(2, 4);
 
-	//System.out.println("0: "+matrice.tab[0][0]);
-    }
+      matrice.print();
+
+      int[] copytab = {1, 5, 54, -10};
+
+      System.out.println("After copy: ");
+      matrice.copy(copytab, 1);
+      matrice.print();
+
+      Matrix add;
+      add = new Matrix(2, 4);
+      System.out.println("Add tab: ");
+      add.print();
+      matrice.addition(add.tab);
+      System.out.println("After addition: ");
+      matrice.print();
+   }
 }
