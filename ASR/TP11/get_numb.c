dc18ed13a1371b77efa66a1cba1ed7cc2cf3d3f0
Matthieu <matthieu-petit@laposte.net>
2018-03-15 10:01:27 +0100

    auto-commit raspberry

--- ASR/TP11/get_numb.c
+++ ASR/TP11/get_numb.c
@@ -5,28 +5,41 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void main (int argc, char *argv[]) {
-  if(argc < 2) {
+int main (int argc, char* argv[]){
+  int fd,n,x, i, offset;
+  off_t l;
+
+  if(argc<3){
     fprintf(stderr, "Usage: %s <file_name> <offset>\n", argv[0]);
     exit(1);
   }
-  int fd, n, r, buf[256], x;
-  fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  fd= open(argv[1], O_RDONLY);
+  if(fd==-1){
+    perror("open fails");
+    exit(2);
+  }
+  i=strtol(argv[2], NULL,0);
+  offset = i * sizeof(int);
+  /* (*) On peut utiliser pour cela la fonction strtol (string to long)
+     i=strtol(argv[2](la chaine de caractère à convertir), NULL,0(3ème argument de strtol: la base dans laquelle il aut lire ce qu'on doit convertir)) 
+  */
 
-  if(n == -1) {
-    perror("read of file fails");
+  //Deplacement
+  l=lseek(fd, offset, SEEK_SET);
+  if(l==-1){
+    perror("lseek fails");
     exit(3);
   }
-  
-  do {
-    write(1, "Number: ", 9);
-    n = scanf(" %d", &x);
-    
-    r = write(fd, &x, sizeof(int));
-    if(r == -1) {
-      perror("Write fails");
-      exit(3);
-    }
-  } while(n == 1);
+  n=read(fd, &x, sizeof(int));
+  if(n==-1){
+    perror("read fails");
+    exit(4);
+  }
+  if(n==0){
+    printf("offset is out of range!\n");
+  }
+  else{
+    printf("The number at offset %d\n is 0x%08x --> %d\n",i,x,x);
+  }
   exit(0);
 }
